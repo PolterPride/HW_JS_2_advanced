7c8d58d9c93e25ba959bca7b661bef362e9b5182
PolterPride <tykimikk1510@gmail.com>
2022-06-20 00:32:10 +0300

    fix 3.js

--- 3.js
+++ 3.js
@@ -1,6 +1,6 @@
-let string = String()
+let result = String()
 for(let i = 1; i <= 5; i++){
     let stringValue = ':)'
-    string += stringValue
-    console.log(string);
+    result += stringValue
+    console.log(result);
 }
