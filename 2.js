0ed3689e54a2614465c955645da926a60e01fd0f
PolterPride <tykimikk1510@gmail.com>
2022-06-20 21:19:46 +0300

    add some info about tasks

--- 2.js
+++ 2.js
@@ -1,3 +1,4 @@
+// 1*. Преобразовать 1 задачу в функцию, принимающую на вход степень, в которую будет возводиться число 2
 function explonentation(degree){
     let result = 2 ** degree
     return result
--- 4.js
+++ 4.js
@@ -1,3 +1,6 @@
+/*2*. Преобразовать 2 задачу в функцию, принимающую на вход строку, которая и будет выводиться в консоль (как в условии смайлик), а также количество строк для вывода 
+e.g. function printSmile(stroka, numberOfRows)*/
+
 function printSmile(stringValue, numberOfRows){
     let result  = String()
     for(let i = 1; i <= numberOfRows; i++){
